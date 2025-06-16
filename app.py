9fb8a473aae988097ea30a2ebe2cbc13b6394422
davide710 <davidemartinellii2003@gmail.com>
2025-06-16 19:01:18 +0200

    .

--- app.py
+++ app.py
@@ -1,34 +1,35 @@
 import gradio as gr
 
-def display_selected_text(selected_text: gr.SelectData):
+def display_selected_text(evt):
   """
   This function is triggered when text is selected in the Textbox.
-  It returns the highlighted text.
+  It's designed to receive the event data from the .select() method.
 
-  Args:
-    selected_text: An object containing the selected text.
+  A print statement is added for debugging in the Hugging Face Space logs.
   """
-  return selected_text.value
+  # This print statement is our key for debugging!
+  print(f"Event triggered! Selected text: {evt.value}")
+  
+  # The function returns the highlighted text value from the event data.
+  return evt.value
 
 with gr.Blocks() as demo:
   gr.Markdown("## Highlight Text Example")
   gr.Markdown("Highlight any portion of the text in the box below to see the selected text displayed.")
   
-  # The Textbox with some initial text.
-  # The .select() method links the display_selected_text function
-  # to the text selection event.
   input_text = gr.Textbox(
       "Go ahead and highlight some of this text!",
       label="Input Text"
   )
   
-  # A separate Textbox to show the output from the function.
   output_text = gr.Textbox(label="Highlighted Text")
   
-  # The select event triggers the function call.
-  # 'input_text' is the input component, and the function's return
-  # value is sent to the 'output_text' component.
-  input_text.select(display_selected_text, None, output_text)
+  # This connects the select event to our function.
+  # When text is selected in 'input_text', it calls 'display_selected_text'.
+  # The event data (which contains the selected text) is automatically
+  # passed as the 'evt' argument to our function.
+  # The function's return value is then placed in 'output_text'.
+  input_text.select(fn=display_selected_text, inputs=None, outputs=output_text)
 
 # Launch the Gradio app
 demo.launch()
